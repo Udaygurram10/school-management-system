80a515a7a822bc7b6d76f9edbfe63a5b5cd3280a
Uday Gurram <148441798+Udaygurram10@users.noreply.github.com>
2025-05-12 23:56:32 +0530

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,22 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
-  swcMinify: false, // Disable SWC minifier
+  swcMinify: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "fs": false,
+      };
+    }
+
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
